c46182fd6f422bf2a67801c266acbce83befd759
Michael Lynn <pudquick@sogetthis.com>
2011-10-20 13:21:16 -0700

    Tidying up

--- pymaCursor.py
+++ pymaCursor.py
@@ -14,13 +14,10 @@ PYMAC_SOURCE_REF = CGEventSourceCreate(kCGEventSourceStateHIDSystemState)
 def getMouseLoc():
 	mouseEvent = CGEventCreate(NULL)
 	mouseLoc = CGEventGetLocation(mouseEvent)
-	# print mouseLoc
 	return mouseLoc
 
 def mouseEvent(eventVal, mouseLocation = False):
-	if (mouseLocation == False):
-		mouseLocation = getMouseLoc()
-	# return CGEventCreateMouseEvent(NULL, eventVal, mouseLocation, 0)
+	if (mouseLocation == False): mouseLocation = getMouseLoc()
 	return CGEventCreateMouseEvent(PYMAC_SOURCE_REF, eventVal, mouseLocation, 0)
 
 def doEvent(eventObj):
@@ -31,18 +28,14 @@ def doEvent(eventObj):
 def performLeftClick(modKeys = 0):
 	mLoc = getMouseLoc()
 	clickMouse = mouseEvent(kCGEventLeftMouseDown, mLoc)
-	if (modKeys != 0):
-		CGEventSetFlags(clickMouse, modKeys)
+	if (modKeys != 0): CGEventSetFlags(clickMouse, modKeys)
 	doEvent(clickMouse)
 	doEvent(mouseEvent(kCGEventLeftMouseUp, mLoc))
-	return True
 
 def performRightClick():
 	mLoc = getMouseLoc()
 	doEvent(mouseEvent(kCGEventRightMouseDown, mLoc))
-	# eventually add in modifiers
 	doEvent(mouseEvent(kCGEventRightMouseUp, mLoc))
-	return True
 
 def performDoubleLeftClick():
 	mLoc = getMouseLoc()
@@ -60,38 +53,29 @@ def performDoubleLeftClick():
 	releaseMouse2 = mouseEvent(kCGEventLeftMouseUp, mLoc)
 	CGEventSetIntegerValueField(releaseMouse2, kCGMouseEventClickState, 1)
 	doEvent(releaseMouse2)
-	return True
 
 def getModKeysValue(doShiftDown = False, doCommandDown = False, doOptionDown = False, doControlDown = False):
 	modKeys = 0
-	if (doShiftDown):
-		modKeys |= kCGEventFlagMaskShift
-	if (doCommandDown):
-		modKeys |= kCGEventFlagMaskCommand
-	if (doOptionDown):
-		modKeys |= kCGEventFlagMaskAlternate
-	if (doControlDown):
-		modKeys |= kCGEventFlagMaskControl
+	if (doShiftDown):   modKeys |= kCGEventFlagMaskShift
+	if (doCommandDown): modKeys |= kCGEventFlagMaskCommand
+	if (doOptionDown):  modKeys |= kCGEventFlagMaskAlternate
+	if (doControlDown): modKeys |= kCGEventFlagMaskControl
 	return modKeys
 
 def allModifiersUp():
-	try:
-		os.system('/usr/bin/osascript -e "tell application \\"System Events\\" to key up {shift, command, option, control}"')
-	finally:
-		return True
+	try: os.system('/usr/bin/osascript -e "tell application \\"System Events\\" to key up {shift, command, option, control}"')
+	finally: return
 
 # move mouse
 
 def moveMouseToPoint(x, y):
 	xFloat,yFloat = (0. + x), (0. + y)
 	doEvent(mouseEvent(kCGEventMouseMoved, mouseLocation=CGPointMake(xFloat, yFloat)))
-	return True
 
 def stepMouseToPoint(x, y, numSteps=1):
 	currentLoc = getMouseLoc()
 	xFloat,yFloat = (0. + x), (0. + y)
-	if (numSteps < 1):
-		numSteps = 1
+	if (numSteps < 1): numSteps = 1
 	xIncrement = (xFloat - currentLoc.x) / numSteps
 	yIncrement = (yFloat - currentLoc.y) / numSteps
 	xPrevious,yPrevious = (0. + currentLoc.x), (0. + currentLoc.y)
@@ -105,10 +89,10 @@ def stepMouseToPoint(x, y, numSteps=1):
 def mouseLocation(isTopCoordinates = True):
 	if (isTopCoordinates):
 		mLoc = getMouseLoc()
-		return "%.0f\n%.0f" % (mLoc.x,mLoc.y)
+		return (mLoc.x,mLoc.y)
 	else:
 		mLoc = NSEvent.mouseLocation()
-		return "%.0f\n%.0f" % (mLoc.x,mLoc.y)
+		return (mLoc.x,mLoc.y)
 
 # helpers
 
@@ -116,6 +100,5 @@ def isPointOnAScreen(point):
 	screens = NSScreen.screens()
 	count = screens.count()
 	for i in range(count):
-		if (NSPointInRect(point, screens[i].frame())):
-			return (True, screens[i])
-	return (False, None)
+		if (NSPointInRect(point, screens[i].frame())): return screens[i]
+	return None
